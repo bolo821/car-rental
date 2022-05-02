ea8d1c5e3552755aeb11386ef01b75401a269f6d
Bolo821 <glorybolo@outlook.com>
2022-05-02 09:21:22 +0300

    update

--- frontend/src/layout/navbar/index.js
+++ frontend/src/layout/navbar/index.js
@@ -93,7 +93,7 @@ const DemoNavbar = () => {
                   </NavLink>
                 </NavItem>
                 {
-                  history.location.pathname.includes('landing') &&
+                  history.location.pathname.includes('clickid') &&
                   <>
                     <NavItem>
                       <NavLink
